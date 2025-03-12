cc50864547252cda0cb603601729bec5420fc6cd
AronVilla <arondustin22@gmail.com>
2025-03-12 21:03:16 +0800

    commit by aron

--- lib/bluetooth_screen.dart
+++ lib/bluetooth_screen.dart
@@ -37,3 +37,44 @@ class _BluetoothScreenState extends State<BluetoothScreen> {
       child: _bluetoothSettings(),
     );
   }
+
+  //commit by aron
+  Widget _bluetoothSettings() {
+    return ListView(
+      children: [
+        const SizedBox(height: 10),
+        _bluetoothHeader(),
+        const SizedBox(height: 10),
+        CupertinoListSection.insetGrouped(
+          children: [
+            CupertinoListTile(
+              title: const Text("Bluetooth"),
+              trailing: isBluetoothSwitchLoading
+                  ? const CupertinoActivityIndicator(radius: 10)
+                  : CupertinoSwitch(
+                      value: isBluetoothEnabled,
+                      onChanged: (bool value) {
+                        setState(() {
+                          isBluetoothSwitchLoading = true;
+                        });
+
+                        Future.delayed(const Duration(milliseconds: 500), () {
+                          setState(() {
+                            isBluetoothSwitchLoading = false;
+                            isBluetoothEnabled = value;
+                            isSearching = value; // Start searching when turned on
+                          });
+                        });
+                      },
+                    ),
+            ),
+            if (isBluetoothEnabled)
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+                child: Text(
+                  "This iPhone is discoverable as 'iPhone' while Bluetooth Settings is open.",
+                  style: TextStyle(fontSize: 14, color: CupertinoColors.systemGrey),
+                ),
+              ),
+          ],
+        ),
